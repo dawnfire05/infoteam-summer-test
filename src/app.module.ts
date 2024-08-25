d2ab877502b608aa198bf3a04eb48f2c13426978
dawnfire05 <dawnfire05@naver.com>
2024-08-25 22:01:42 +0900

    feat: created init module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './user/auth/auth.module';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { PostModule } from './post/post.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule, PostModule],
   controllers: [AppController],
   providers: [AppService],
 })
